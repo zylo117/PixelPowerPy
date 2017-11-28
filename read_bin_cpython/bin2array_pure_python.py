08c742531b7371f361efad44cde409cc79cdd42b
zylo117 <zylo117@hotmail.com>
2017-11-28 18:04:34 +0800

    correct value

--- read_bin_cpython/bin2array_pure_python.py
+++ read_bin_cpython/bin2array_pure_python.py
@@ -1,6 +1,9 @@
 import argparse
 import datetime
-import pyximport;pyximport.install()
+import numpy
+import pyximport;
+
+pyximport.install()
 import bin2array_c
 
 ap = argparse.ArgumentParser()
@@ -13,9 +16,20 @@ raw_dec = [];
 time1 = datetime.datetime.now()
 
 raw_dec = bin2array_c.b2a(args["image"])
+print(len(raw_dec))
 
 time2 = datetime.datetime.now()
 
 print(time2 - time1)
 
+width = raw_dec[0]
+height = raw_dec[1]
 
+raw_dec = numpy.array(raw_dec)
+
+raw_dec = raw_dec[2:]
+
+raw_dec = raw_dec.reshape((height, width))
+
+print(width, height)
+print(raw_dec[0][0], raw_dec[0][1], raw_dec[1][0], raw_dec[1][1])
