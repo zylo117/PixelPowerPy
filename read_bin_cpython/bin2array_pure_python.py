62d2eff742b8ff947e20f9fa03984d9e94b07bba
zylo117 <zylo117@hotmail.com>
2017-11-29 16:53:41 +0800

    whitebalance works

--- read_bin_cpython/bin2array_pure_python.py
+++ read_bin_cpython/bin2array_pure_python.py
@@ -2,7 +2,7 @@ import argparse
 import datetime
 import array
 import numpy
-import preprocess
+from funtset import *
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to the raw image")
@@ -26,52 +26,17 @@ raw = raw.reshape((height, width))
 print(raw[0][0], raw[0][1], raw[1][0], raw[1][1])
 
 # 按mode切片
-if args["mode"] is 1:
-    raw = raw[8:height - 8, 8:width - 8]
-elif args["mode"] is 2:
-    raw = raw[int((height - (width - 16) / (16 / 9)) / 2): height - int((height - (width - 16) / (16 / 9)) / 2),
-          8:width - 8]
-elif args["mode"] is 3:
-    raw = raw[8:height - 8,
-          int((width - (height - 16) * (3 / 2)) / 2): width - int((width - (height - 16) * (3 / 2)) / 2)]
-elif args["mode"] is 4:
-    raw = raw[8:height - 8,
-          int((width - (height - 16) * (4 / 3)) / 2): width - int((width - (height - 16) * (4 / 3)) / 2)]
+raw = crop_by_mode(raw, args["mode"])
 
-# 更新宽高
-width = raw.shape[1]
-height = raw.shape[0]
-
-# 调用preprocess（C编译）辅助
-raw = preprocess.pedestal(args["pedestal"], raw)
+# 增益
+raw = raw + [args["pedestal"]]
 print(raw[0][0], raw[0][1], raw[1][0], raw[1][1])
 
-# 分4层
-plane = numpy.zeros((int(height / 2), int(width / 2), 4))
-plane[:, :, 0] = raw[::2, ::2]
-plane[:, :, 1] = raw[::2, 1::2]
-plane[:, :, 2] = raw[1::2, ::2]
-plane[:, :, 3] = raw[1::2, 1::2]
-
-print(plane[0][0][0], plane[0][1][0], plane[1][0][0], plane[1][1][0])
-print(plane[0][0][1], plane[0][1][1], plane[1][0][1], plane[1][1][1])
-print(plane[0][0][2], plane[0][1][2], plane[1][0][2], plane[1][1][2])
-print(plane[0][0][3], plane[0][1][3], plane[1][0][3], plane[1][1][3])
-
-block_size_R = 100
-block_size_C = 100
-center = [plane.shape[0] / 2 - 1, plane.shape[1] / 2 - 1]
-
-center_block = plane[int(center[0] - block_size_R / 2 + 1):int(center[0] + int(block_size_R / 2 + 1)),
-               int(center[1] - int(block_size_C / 2) + 1): int(center[1] + int(block_size_C / 2) + 1), :]
-
-center_mean = numpy.mean((numpy.mean(center_block, axis=0)), axis=0)
-balance = numpy.max(center_mean) / center_mean
-
-# 调用preprocess（C编译）辅助
-for i in range(4):
-    plane[:, :, i] = plane[:, :, i] * balance[i]
+#白平衡
+raw = white_balance(raw)
 
 time2 = datetime.datetime.now()
 
 print(time2 - time1)
+
+
---
+++ read_bin_cpython/funtset.py
@@ -0,0 +1,66 @@
+import numpy
+
+def crop_by_mode(raw, mode):
+    # 更新宽高
+    width = raw.shape[1]
+    height = raw.shape[0]
+
+    # 按mode切片
+    if mode is 1:
+        raw = raw[8:height - 8, 8:width - 8]
+    elif mode is 2:
+        raw = raw[int((height - (width - 16) / (16 / 9)) / 2): height - int((height - (width - 16) / (16 / 9)) / 2),
+              8:width - 8]
+    elif mode is 3:
+        raw = raw[8:height - 8,
+              int((width - (height - 16) * (3 / 2)) / 2): width - int((width - (height - 16) * (3 / 2)) / 2)]
+    elif mode is 4:
+        raw = raw[8:height - 8,
+              int((width - (height - 16) * (4 / 3)) / 2): width - int((width - (height - 16) * (4 / 3)) / 2)]
+
+    return raw
+
+def white_balance(raw):
+    # 更新宽高
+    width = raw.shape[1]
+    height = raw.shape[0]
+
+    # 分4层
+    plane = numpy.zeros((int(height / 2), int(width / 2), 4))
+    plane[:, :, 0] = raw[::2, ::2]
+    plane[:, :, 1] = raw[::2, 1::2]
+    plane[:, :, 2] = raw[1::2, ::2]
+    plane[:, :, 3] = raw[1::2, 1::2]
+
+    print(plane[0][0][0], plane[0][1][0], plane[1][0][0], plane[1][1][0])
+    print(plane[0][0][1], plane[0][1][1], plane[1][0][1], plane[1][1][1])
+    print(plane[0][0][2], plane[0][1][2], plane[1][0][2], plane[1][1][2])
+    print(plane[0][0][3], plane[0][1][3], plane[1][0][3], plane[1][1][3])
+
+    block_size_R = 100
+    block_size_C = 100
+    center = [plane.shape[0] / 2 - 1, plane.shape[1] / 2 - 1]
+
+    center_block = plane[int(center[0] - block_size_R / 2 + 1):int(center[0] + int(block_size_R / 2 + 1)),
+                   int(center[1] - int(block_size_C / 2) + 1): int(center[1] + int(block_size_C / 2) + 1), :]
+
+    center_mean = numpy.mean((numpy.mean(center_block, axis=0)), axis=0)
+    balance = numpy.max(center_mean) / center_mean
+
+    for i in range(4):
+        plane[:, :, i] = plane[:, :, i] * balance[i]
+
+    print(plane[0][0][0], plane[0][1][0], plane[1][0][0], plane[1][1][0])
+    print(plane[0][0][1], plane[0][1][1], plane[1][0][1], plane[1][1][1])
+    print(plane[0][0][2], plane[0][1][2], plane[1][0][2], plane[1][1][2])
+    print(plane[0][0][3], plane[0][1][3], plane[1][0][3], plane[1][1][3])
+
+    # 4合1
+    post_wb = numpy.zeros(raw.shape)
+    post_wb[::2, ::2] = plane[:, :, 0]
+    post_wb[::2, 1::2] = plane[:, :, 1]
+    post_wb[1::2, ::2] = plane[:, :, 2]
+    post_wb[1::2, 1::2] = plane[:, :, 3]
+    print(post_wb[0][0], post_wb[0][1], post_wb[1][0], post_wb[1][1])
+
+    return post_wb
