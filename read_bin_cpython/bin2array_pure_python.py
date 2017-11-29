803e2a3dda8775b9ed9b3b599912591cdea12a47
zylo117 <zylo117@hotmail.com>
2017-11-29 16:30:30 +0800

    add cython mod

--- read_bin_cpython/bin2array_pure_python.py
+++ read_bin_cpython/bin2array_pure_python.py
@@ -2,10 +2,6 @@ import argparse
 import datetime
 import array
 import numpy
-
-import pyximport;
-
-pyximport.install()
 import preprocess
 
 ap = argparse.ArgumentParser()
@@ -31,18 +27,22 @@ print(raw[0][0], raw[0][1], raw[1][0], raw[1][1])
 
 # 按mode切片
 if args["mode"] is 1:
-    raw = raw[8:height - 16 - 1, 8:width - 16 - 1]
+    raw = raw[8:height - 8, 8:width - 8]
 elif args["mode"] is 2:
-    raw = raw[int((height - (width - 16) / (16 / 9)) / 2): height - int((height - (width - 16) / (16 / 9)) / 2), 8:width - 8]
+    raw = raw[int((height - (width - 16) / (16 / 9)) / 2): height - int((height - (width - 16) / (16 / 9)) / 2),
+          8:width - 8]
 elif args["mode"] is 3:
-    raw = raw[8:height - 16 - 1, 8:width - 16 - 1]
+    raw = raw[8:height - 8,
+          int((width - (height - 16) * (3 / 2)) / 2): width - int((width - (height - 16) * (3 / 2)) / 2)]
 elif args["mode"] is 4:
-    raw = raw[8:height - 16 - 1, 8:width - 16 - 1]
+    raw = raw[8:height - 8,
+          int((width - (height - 16) * (4 / 3)) / 2): width - int((width - (height - 16) * (4 / 3)) / 2)]
 
 # 更新宽高
 width = raw.shape[1]
 height = raw.shape[0]
 
+# 调用preprocess（C编译）辅助
 raw = preprocess.pedestal(args["pedestal"], raw)
 print(raw[0][0], raw[0][1], raw[1][0], raw[1][1])
 
@@ -58,14 +58,20 @@ print(plane[0][0][1], plane[0][1][1], plane[1][0][1], plane[1][1][1])
 print(plane[0][0][2], plane[0][1][2], plane[1][0][2], plane[1][1][2])
 print(plane[0][0][3], plane[0][1][3], plane[1][0][3], plane[1][1][3])
 
-time2 = datetime.datetime.now()
-
-print(time2 - time1)
-
 block_size_R = 100
 block_size_C = 100
 center = [plane.shape[0] / 2 - 1, plane.shape[1] / 2 - 1]
 
-a = plane[int(center[0] - block_size_R / 2 + 1):int(center[0] + int(block_size_R / 2 + 1)), int(center[1] - int(block_size_C / 2) + 1): int(center[1] + int(block_size_C / 2) + 1), :]
+center_block = plane[int(center[0] - block_size_R / 2 + 1):int(center[0] + int(block_size_R / 2 + 1)),
+               int(center[1] - int(block_size_C / 2) + 1): int(center[1] + int(block_size_C / 2) + 1), :]
 
+center_mean = numpy.mean((numpy.mean(center_block, axis=0)), axis=0)
+balance = numpy.max(center_mean) / center_mean
 
+# 调用preprocess（C编译）辅助
+for i in range(4):
+    plane[:, :, i] = plane[:, :, i] * balance[i]
+
+time2 = datetime.datetime.now()
+
+print(time2 - time1)
--- read_bin_cpython/preprocess.pyx
+++ read_bin_cpython/preprocess.pyx
@@ -10,8 +10,26 @@ cpdef unsigned short[:, :] pedestal(int value, unsigned short[:, :] raw_image):
     # loop over the image
     for y in range(0, h):
         for x in range(0, w):
-            # threshold the pixel
+            # 增益
             raw_image[y, x] += value
+
+    # return image
+    return raw_image
+
+# 图片白平衡辅助
+cpdef unsigned short[:, :] whitebalance(unsigned short[:, :] raw_image):
+    # set the variable extension types
+    cdef int x, y, w, h
+
+    # grab the image dimensions
+    h = raw_image.shape[0]
+    w = raw_image.shape[1]
+
+    # loop over the image
+    for y in range(0, h):
+        for x in range(0, w):
+            # 遍历，每个像素乘平衡因子
+            raw_image[y, x] += 1
 
     # return image
     return raw_image
