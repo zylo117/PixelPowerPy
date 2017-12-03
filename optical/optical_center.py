9ebdcae9db4d2da6264bec04d0c618d6df2f3693
zylo117 <zylo117@hotmail.com>
2017-12-03 17:08:32 +0800

    oc finished

--- optical/optical_center.py
+++ optical/optical_center.py
@@ -26,4 +26,25 @@ def oc(IDraw, bayerformat="rggb", pedestal=64, bitdepth=10):
         y1 = int(roi_center_y[i] - (roi_size_half - 0.5))
         y2 = int(roi_center_y[i] + (roi_size_half - 0.5))
         threshold_data[i] = np.mean(IDy[y1:y2 + 1, x1:x2 + 1])
+
+    # 计算并应用二值化（threshold）
+    oc_threshold = np.mean([threshold_data[0], np.mean(threshold_data[1:])])  # 求出二值化的阀值
+    ID_threshold_binary = (IDy >= oc_threshold).astype(np.double)  # 浮点布尔图，False为暗，True为亮
+
+    # 计算重心
+    total_points = int(np.sum(ID_threshold_binary))
+    row_weight = np.arange(1, h + 1)
+    col_weight = np.arange(1, w + 1)
+    row_sum = np.sum(ID_threshold_binary, axis=1) * row_weight
+    col_sum = np.sum(ID_threshold_binary, axis=0) * col_weight
+
+    oc_x = np.sum(col_sum) / total_points
+    oc_y = np.sum(row_sum) / total_points
+    oc_x_shift = oc_x - (IDy.shape[1] / 2 + 0.5)
+    oc_y_shift = (IDy.shape[0] / 2 + 0.5) - oc_y
+
+    # 半径
+    oc_mag_shift = np.sqrt(oc_x_shift**2 + oc_y_shift**2)
+
+    output = [oc_threshold, oc_x, oc_x_shift, oc_y, oc_y_shift, oc_mag_shift]
     print()
